6c6476fae82c302202243d348d321815256444ac
Kunle Adeyinka <aadeyink@gmail.com>
2020-11-19 19:04:30 -0500

    added exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState([])
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -27,9 +31,26 @@ const App = () => {
     setNewPhone(e.target.value)
   }
 
+  const handleKeyUp = (e) => {
+    const searchQry = e.target.value
+    const filteredPersons = persons.filter(
+      person => {
+        return person.name.toLowerCase().includes(searchQry)
+      }
+    )
+    setFilteredPersons(filteredPersons)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onKeyUp={handleKeyUp} />
+        <br /><br />
+        {filteredPersons.map((filteredPerson) => <p key={filteredPerson.name}>{filteredPerson.name}{' '} {filteredPerson.number}</p>)}
+        <br /><br />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input onChange={changeName} />
@@ -44,7 +65,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}{' '} {person.phone}</p>)}
+      {persons.map((person) => <p key={person.name}>{person.name}{' '} {person.number}</p>)}
     </div>
   )
 }
